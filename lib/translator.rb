64417aad702c0162f56e91236c80fe8661de92f0
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-22 14:02:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,6 +19,7 @@ end
 
 def get_japanese_emoticon(file, japanese_emoticon)
   outter_most_hash = load_library(file)
+  result = english_word
   outter_most_hash.each do |english_word, symbol_pair|
     symbol_pair.each do |symbol|
       binding.pry
