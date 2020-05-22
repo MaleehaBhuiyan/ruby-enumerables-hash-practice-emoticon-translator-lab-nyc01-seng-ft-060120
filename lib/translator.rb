529d496c5c1d3f94c9586b9c91cd77a3c50c9d55
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-22 15:24:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,8 +20,9 @@ end
 def get_japanese_emoticon(file, japanese_emoticon)
   outter_most_hash = load_library(file)
   response = nil
+  binding.pry 
   outter_most_hash[english_word].each do |english_word, symbol_pair|
-    if japanese_emoticon == symbol_pair 
+    if japanese_emoticon == symbol_pair
       response = english_word
     end
   end
