b37fa8f2a450eca6f40609d120e09519fcf7ec56
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-22 14:22:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,5 +1,5 @@
 # require modules here
-require 'pry'
+=begin require 'pry'
 require 'yaml'
 
 
@@ -31,10 +31,57 @@ def get_japanese_emoticon(file, japanese_emoticon)
     return response
   end
 end
-binding.pry
-
 
 def get_english_meaning(file, emoticon)
   outter_most_hash = load_library(file)
 
 end
+=end 
+
+require 'yaml'
+# require modules here
+
+def load_library
+  # code goes here
+def load_library(yaml_file)
+  response = {"get_meaning" => {}, "get_emoticon" => {}}
+  library = YAML.load_file(yaml_file)
+  library.each do |trans, emos|
+    response["get_meaning"][emos[1]] = trans
+    response["get_emoticon"][emos[0]] = emos[1]
+  end
+  response
+end
+
+def get_japanese_emoticon
+  # code goes here
+def get_japanese_emoticon(yaml_file, emoticon)
+  library = load_library(yaml_file)
+  response = nil
+  library["get_emoticon"].each do |english, japanese|
+    if emoticon == english
+      response = japanese
+    end
+  end
+  if response == nil
+    return "Sorry, that emoticon was not found"
+  else
+    return response
+  end
+end
+
+def get_english_meaning
+  # code goes here
+def get_english_meaning(yaml_file, emoticon)
+  library = load_library(yaml_file)
+  response = nil
+  library["get_meaning"].each do |emo, trans|
+    if emo == emoticon
+      response = trans
+    end
+  end
+  if response == nil
+    return "Sorry, that emoticon was not found"
+  else
+    return response
+  end
